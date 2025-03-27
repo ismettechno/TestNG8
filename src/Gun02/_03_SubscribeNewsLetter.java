28d73288bb6493f427c293bf874cdc4c21241ec7
TechnoStudy <ismettechno@gmail.com>
2025-03-27 17:22:31 +0000

    BaseDriver a login eklendi ve SubscribeFunctionYes yapıldı

--- src/Gun02/_03_SubscribeNewsLetter.java
+++ src/Gun02/_03_SubscribeNewsLetter.java
@@ -1,11 +1,32 @@
 package Gun02;
 
-public class _03_SubscribeNewsLetter {
+import Utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _03_SubscribeNewsLetter extends BaseDriver {
     /*
     Senaryo
   1- Siteyi açınız.
   2- Newsletter  Subscribe işlemini abone olunuz(YES)  , işlemin başarılı olduğunu kontrol ediniz.
-
-
      */
+
+    @Test
+    public void SubscribeFunctionYes()
+    {
+        WebElement newsLetterLink=driver.findElement(By.linkText("Newsletter"));
+        newsLetterLink.click();
+
+        WebElement subscribeYes=driver.findElement(By.xpath("//input[@type='radio' and @value='1']"));
+        subscribeYes.click();
+
+        WebElement continueButton=driver.findElement(By.xpath("//*[@value='Continue']"));
+        continueButton.click();
+
+        WebElement msgLabel=driver.findElement(By.cssSelector("[class='alert alert-success alert-dismissible']"));
+        Assert.assertTrue(msgLabel.getText().toLowerCase().contains("success"));
+    }
+
 }
--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -1,9 +1,13 @@
 package Utility;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
@@ -21,7 +25,34 @@ public class BaseDriver {
         driver.manage().window().maximize(); // Ekranı max yapıyor.
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30)); // 20 sn mühlet: sayfayı yükleme mühlet
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); // 5 sn mühlet: elementi bulma mühleti
+
+        LoginTest();
     }
+
+
+    public void LoginTest()
+    {
+        System.out.println("Login Test başladı");
+        driver.get("http://opencart.abstracta.us/index.php?route=account/login");
+        MyFunc.Bekle(2);
+
+        WebElement email=driver.findElement(By.xpath("//input[@id='input-email']"));
+        email.sendKeys("testng1@gmail.com");
+
+        WebElement password=driver.findElement(By.xpath("//input[@id='input-password']"));
+        password.sendKeys("123qweasd");
+
+        WebElement loginBtn=driver.findElement(By.xpath("//*[@value='Login']"));
+        loginBtn.click();
+
+        driver.findElement(By.xpath("//*[@id='details-button']")).click();
+        driver.findElement(By.xpath("//*[@id='proceed-link']")).click();
+
+        Assert.assertTrue(driver.getTitle().equals("My Account"), "Login olunamadı");
+
+        System.out.println("Login Test bitti");
+    }
+
 
     @AfterClass
     public void TearDown()
