16eda68a39bc16e0cb2db23b33fcd29088b8b49d
TechnoStudy <ismettechno@gmail.com>
2025-03-31 18:14:20 +0100

    GroupsExcludeMetod.xml

--- src/Gun03/_01_Groups.java
+++ src/Gun03/_01_Groups.java
@@ -1,27 +1,35 @@
 package Gun03;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class _01_Groups {
+    //@Test(groups = { "group1", "group2" })  çoklu group
 
-    @Test
+    @Test(groups={"Smoke Test"})
     public void Test1() { System.out.println("test1"); }
 
-    @Test
+    @Test(groups = {"Regression"})
     public void Test3() { System.out.println("test3"); }
 
-    @Test
+    @Test(groups = {"Regression"})
     public void Test5() { System.out.println("test5"); }
 
-    @Test
+    @Test(groups = {"Regression"})
     public void Test2() { System.out.println("test2"); }
 
-    @Test
+    @Test(groups={"Smoke Test"})
     public void Test4() { System.out.println("test4"); }
 
-    @Test
+    @Test(groups={"Smoke Test"})
     public void Test6() { System.out.println("test6"); }
 
     @Test
     public void Test7() { System.out.println("test7"); }
+
+    @BeforeGroups(groups={"Smoke Test", "Regression" })
+    public void bGroups() { System.out.println("Before Groups"); }
+
+    @AfterGroups(groups={"Smoke Test", "Regression" })
+    public void aGroups() { System.out.println("After Groups"); }
+
 }
