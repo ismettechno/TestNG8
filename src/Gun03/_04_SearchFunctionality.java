93f60fc429173b7d705443a64bd8447734584c85
TechnoStudy <ismettechno@gmail.com>
2025-03-31 19:50:12 +0100

    _04_SearchFunctionality soru çözüm

--- src/Gun03/_04_SearchFunctionality.java
+++ src/Gun03/_04_SearchFunctionality.java
@@ -8,5 +8,32 @@ package Gun03;
       4- aynı işlemi samsung(xml den gönderilecek için de yapınız
     */
 
-public class _04_SearchFunctionality {
+import Utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class _04_SearchFunctionality extends BaseDriver {
+
+    @Test
+    @Parameters("searchText")
+    public void SearchFunction(String aranacakText)
+    {
+        WebElement searchInput=driver.findElement(By.name("search"));
+        searchInput.sendKeys(aranacakText+ Keys.ENTER);
+
+        List<WebElement> captions=driver.findElements(By.xpath("//*[@class='caption']/h4/a"));  //  //*[@class='caption']
+
+        for(WebElement e: captions) {
+            System.out.println("e.getText() = " + e.getText());
+            Assert.assertTrue(e.getText().toLowerCase().contains(aranacakText.toLowerCase()));
+        }
+
+    }
+
 }
