b565140b34daa6d56fc763e63eeb6ec59270f79a
TechnoStudy <ismettechno@gmail.com>
2025-03-31 19:24:40 +0100

    _04_SearchFunctionality soru fix

--- src/Gun03/_04_SearchFunctionality.java
+++ src/Gun03/_04_SearchFunctionality.java
@@ -3,9 +3,9 @@ package Gun03;
 /*
       Senaryo ;
       1- Siteyi açınız.
-      2- mac kelimeini göndererek aratınız.
+      2- mac(xml den gönderilecek) kelimeini göndererek aratınız.
       3- Çıkan sonuçlarda mac kelimesinin geçtiğini doğrulayınız.
-      4- aynı işlemi samsung için de yapınız
+      4- aynı işlemi samsung(xml den gönderilecek için de yapınız
     */
 
 public class _04_SearchFunctionality {
