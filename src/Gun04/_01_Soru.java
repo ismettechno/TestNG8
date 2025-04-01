821971866eb269f1daa61fcaf3294902523545b5
TechnoStudy <ismettechno@gmail.com>
2025-04-01 18:26:00 +0100

    _01_Soru Çözüm

--- src/Gun04/_01_Soru.java
+++ src/Gun04/_01_Soru.java
@@ -12,5 +12,37 @@ package Gun04;
 */
 
 
-public class _01_Soru {
+import Utility.BaseDriver;
+import Utility.BaseDriverParameter;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class _01_Soru extends BaseDriverParameter {
+
+    @Test
+    public void SpecialsTest()
+    {
+        WebElement specials=driver.findElement(By.linkText("Specials"));
+        specials.click();
+
+        List<WebElement> newPrices=driver.findElements(By.cssSelector("span[class='price-new']"));
+        List<WebElement> oldPrices=driver.findElements(By.cssSelector("span[class='price-old']"));
+
+        //3.madde
+        Assert.assertTrue(newPrices.size() == oldPrices.size(),"Bütün ürünlerde indirim bulunamadı");
+
+        //4.madde
+        for (int i = 0; i < newPrices.size(); i++) {
+            double newPrice= Double.parseDouble(newPrices.get(i).getText().replaceAll("[^0-9,.]",""));
+            double oldPrice= Double.parseDouble(oldPrices.get(i).getText().replaceAll("[^0-9,.]",""));
+
+            Assert.assertTrue(oldPrice > newPrice, "Yeni fiyat eskisinden küçük değil");
+        }
+
+    }
+
 }
