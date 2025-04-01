abb649132e35e6aa74794c642b109c176ecdc536
TechnoStudy <ismettechno@gmail.com>
2025-04-01 19:40:13 +0100

    _01_Soru 2

--- src/Gun05/_01_Soru.java
+++ src/Gun05/_01_Soru.java
@@ -4,6 +4,11 @@ package Gun05;
   Senaryo;
   1- Brands menusune gidiniz.
   2- Burada her bir markanın üstünde yer alan harf ile başladığını doğrulayınız
+
+  WebElement element=driver.findElement(By.id("btn"));// elementi tum sayafada ara
+
+  Webelement altElement=element.findElement(By.classname("new"));  // yukarıdaki alınan elementin içinde
+                                                                   // arar
 */
 
 public class _01_Soru {
