66b4067caa028a8d4f437c949dd2c73980d6e5fc
TechnoStudy <ismettechno@gmail.com>
2025-04-01 18:34:48 +0100

    _02_Soru

---
+++ src/Gun04/_02_Soru.java
@@ -0,0 +1,16 @@
+package Gun04;
+
+/*
+
+   Senaryo
+   1- Siteye gidiniz
+   2- Your Store tiklayiniz, (Ana sayfaya gidiniz)
+   3- Cikan urun lerde (bütün ürünlerde indirim var mı, eski fiyat yeni fiyat var mı)
+      kontrol edin, indirimde olan ürünlerin miktarını bulunuz.
+   4- Yukarıdaki işlemden sonra indirimde olan ürünleri önceki fiyatının şu andaki fiyattan
+      yüksek olduğunu doğrulayınız.
+
+*/
+
+public class _02_Soru {
+}
