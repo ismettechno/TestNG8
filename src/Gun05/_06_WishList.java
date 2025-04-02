ded6c50677fafd5caa495449d2215d1ff35b4fc4
TechnoStudy <ismettechno@gmail.com>
2025-04-02 19:49:43 +0100

    _06_WishList soru fix

--- src/Gun05/_06_WishList.java
+++ src/Gun05/_06_WishList.java
@@ -3,10 +3,11 @@ package Gun05;
 /*
     Senaryo ;
     1- Siteye gidiniz..
-    2- "ipod" ürününü aratınız."ipod" u xml den gönderiniz
+    2- "ipod" ürününü aratınız.
     3- çıkan elamnlardan random bir elelamnı Add Wish butonuna tıklayınız.
     4- Daha sonra WishList e tıklatınız
     5- Burada çıkan ürünle tıklanan ürünün isminin aynı olup olmadığını doğrulayınız.
+    (POM olmadan çözünüz)
  */
 
 public class _06_WishList {
