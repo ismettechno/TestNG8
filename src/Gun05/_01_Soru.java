c03c8d298b6fa84130d573a48f95d874c2a1a60f
TechnoStudy <ismettechno@gmail.com>
2025-04-02 17:45:21 +0100

    _01_Soru çözüm

--- src/Gun05/_01_Soru.java
+++ src/Gun05/_01_Soru.java
@@ -9,7 +9,50 @@ package Gun05;
 
   Webelement altElement=element.findElement(By.classname("new"));  // yukarıdaki alınan elementin içinde
                                                                    // arar
+
+  harfler= [id='content'] > h2
+  markalar = [id='content'] > div
+
 */
 
-public class _01_Soru {
+
+import Utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class _01_Soru extends BaseDriver {
+
+    @Test
+    public void BrandsTest()
+    {
+        WebElement brandsLink=driver.findElement(By.linkText("Brands"));
+        brandsLink.click();
+
+        List<WebElement> harfler=driver.findElements(By.cssSelector("[id='content'] > h2"));
+        List<WebElement> markalar=driver.findElements(By.cssSelector("[id='content'] > div"));
+
+        for (int i = 0; i < harfler.size(); i++) {
+            System.out.println("harfler.get(i).getText() = " + harfler.get(i).getText());
+
+            //marka bloklarındaki div leri alarak markalara tek tek ulaşabildim, list olarak
+            List<WebElement> harfinMarkalari= markalar.get(i).findElements(By.tagName("div"));
+             
+            for (WebElement marka: harfinMarkalari) {
+                System.out.println("marka.getText() = " + marka.getText());
+
+                //markalar harfle mi başlıyor
+                Assert.assertEquals(harfler.get(i).getText().charAt(0),
+                                    marka.getText().charAt(0),
+                                    "Marka harfle başlamıyor"
+                                    );
+            }
+        }
+    }
+
+// TODO: aynı soruyu  https://www.ebay.com/n/all-brands bu sitesinden test edelim. %100
+
 }
