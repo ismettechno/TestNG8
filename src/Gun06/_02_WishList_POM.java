4c58a1f0d16facfac3dd643adc5289cba47f16c2
TechnoStudy <ismettechno@gmail.com>
2025-04-03 18:30:04 +0100

    _03_AddressFunctionality soru

--- src/Gun06/_02_WishList_POM.java
+++ src/Gun06/_02_WishList_POM.java
@@ -11,7 +11,43 @@ package Gun06;
  */
 
 
-public class _02_WishList_POM {
+import Gun05._04_PlaceOrder_Elementleri;
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class _02_WishList_POM extends BaseDriver {
+
+    @Test
+    public void addToWishList() {
+
+        _04_PlaceOrder_Elementleri poe=new _04_PlaceOrder_Elementleri();
+        _02_WishList_Elements wle=new _02_WishList_Elements();
+
+
+        poe.searchBox.sendKeys("ipod"+ Keys.ENTER);
+
+        int rndSayi= MyFunc.RandomSayiVer(wle.productNameList.size());
+        String  selectedProductName=wle.productNameList.get(rndSayi).getText();
+
+        wle.wishButtons.get(rndSayi).click();
+        wle.wishListLink.click();
+
+        boolean bulundu=false;
+
+        for(WebElement e: wle.wishNameList)
+            if (e.getText().equals(selectedProductName))
+                bulundu=true;
+
+        Assert.assertTrue(bulundu, "WishListe eklenen ürün listede bulunamadı");
+
+    }
 
 
 
---
+++ src/Gun06/_03_AddressFunctionality.java
@@ -0,0 +1,13 @@
+package Gun06;
+
+/*
+       Senaryo;
+       1- Siteyi açınız.
+       2- Adress Ekleyiniz.
+       3- En son adresi edit yaparak ad ve soyadı değiştirip kaydediniz.
+       4- En son adresi siliniz.
+ */
+
+public class _03_AddressFunctionality {
+
+}
